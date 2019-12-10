7fa1a867d902a9007e739e1b632cbd82abede721
yatoogamii <bracaval.elias.pro@gmail.com>
2019-12-10 19:38:47 +0100

    add randomized for cond

--- services/addCharacter.ts
+++ services/addCharacter.ts
@@ -1,12 +1,17 @@
 import {Request, Response, NextFunction} from "express";
 import CharacterModel from '../models/CharacterModel';
 
+function randomizedCond() {
+  const randomNumber = Math.random();
+  return Math.round(randomNumber) === 1 ? true : false;
+}
+
 export default async (req: Request, res: Response, next: NextFunction) => {
   const nameOfNewCharacter = req.body.name;
   const newCharacter = new CharacterModel({
     name: nameOfNewCharacter,
-    cond1: false,
-    cond2: true
+    cond1: randomizedCond(),
+    cond2: randomizedCond()
   })
 
   await newCharacter.save();
